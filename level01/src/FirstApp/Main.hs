78330f10b63b3ebe9533f688d49d800a16242611
Michiel Borkent <michielborkent@gmail.com>
2018-03-21 20:04:24 +0100

    Level 01

--- level01/src/FirstApp/Main.hs
+++ level01/src/FirstApp/Main.hs
@@ -8,6 +8,10 @@ import           Network.Wai.Handler.Warp (run)
 
 import           Network.HTTP.Types       (status200)
 
+import qualified Data.ByteString.Lazy.Char8 as L
+
+import Data.Monoid
+
 -- Our "application" will respond to ALL incoming requests with a 200
 -- status code response and the message "Hello, World!"
 --
@@ -32,8 +36,11 @@ app
   :: Request
   -> (Response -> IO ResponseReceived)
   -> IO ResponseReceived
-app _ cb =
-  error "Application not implemented"
+app r cb =
+  cb $ responseLBS
+        status200
+        [("Content-Type", "text/plain")]
+        ("Hello, Web!" <> L.pack (show r))
 
 -- We keep this main function here as it is useful to build your application as
 -- a library. The reasoning behind this is that when you come to do your
@@ -41,5 +48,5 @@ app _ cb =
 -- needing to worry about any initialisation code you've buried in your
 -- executable Main.hs.
 runApp :: IO ()
-runApp = run undefined undefined
+runApp = run 8080 app
 
